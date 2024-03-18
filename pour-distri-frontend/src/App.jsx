bfc2cc557f314be4a28055abd3e449a43f94bb40
Chibuike Mba <chibuike.code@gmail.com>
2024-03-18 19:29:44 +0100

    Update App.jsx and main.jsx

--- pour-distri-frontend/src/App.jsx
+++ pour-distri-frontend/src/App.jsx
@@ -1,4 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+    HomeLayout,
+    About,
+    DistributorApplication,
+    Landing,
+    SinglePageError,
+    SingleCocktail,
+    GlobalErrorPage,
+} from "./pages";
+
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleCocktailLoader } from "./pages/SingleCocktail";
+import { action as applicationFormAction } from "./pages/DistributorApplication";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomeLayout />,
+        errorElement: <GlobalErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Landing />,
+                loader: landingLoader,
+                errorElement: <SinglePageError />,
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+            {
+                path: "apply",
+                element: <DistributorApplication />,
+                action: applicationFormAction,
+            },
+            {
+                path: "cocktail/:id",
+                element: <SingleCocktail />,
+                loader: singleCocktailLoader,
+                errorElement: <SinglePageError />,
+            },
+        ],
+    },
+]);
+
 const App = () => {
-    return <div>App</div>;
+    return <RouterProvider router={router} />;
 };
 export default App;
--- pour-distri-frontend/src/main.jsx
+++ pour-distri-frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <>
+        <App />
+        <ToastContainer position="top-center" autoClose={3000} />
+    </>
+);
