d7a9662b68bd3aecd6e522756065450c73f15ecc
Chibuike Mba <chibuike.code@gmail.com>
2024-03-18 19:19:38 +0100

    Add the Site's Assets and Data

---
+++ pour-distri-frontend/src/assets/data/about.js
@@ -0,0 +1,36 @@
+export const about = [
+    {
+        title: "About CocktailConduit",
+        content:
+            "Welcome to CocktailConduit, your premier online platform for connecting qualified distributors with a diverse selection of exquisite cocktails from around the world. At CocktailConduit, we're passionate about fostering partnerships and facilitating the distribution of premium cocktails to distributors globally.",
+    },
+
+    {
+        title: "Our Story",
+        content:
+            "Our Story The idea for CocktailConduit was born out of a shared passion for mixology and a vision to bridge the gap between cocktail producers and distributors. The company, recognized the need for a centralized platform that would streamline the distribution process and empower both producers and distributors to thrive in a competitive market. Since our inception, CocktailConduit has grown into a vibrant community of cocktail enthusiasts, producers, and distributors united by a common love for exceptional drinks and unparalleled service.",
+    },
+    {
+        title: "Our Values",
+        content: {
+            lists: [
+                {
+                    listTitle: "Quality",
+                    list: "We are committed to delivering only the finest quality cocktails crafted with premium ingredients and expert craftsmanship.",
+                },
+                {
+                    listTitle: "Collaboration",
+                    list: "We believe in the power of collaboration and strive to foster meaningful partnerships between producers and distributors.",
+                },
+                {
+                    listTitle: "Innovation",
+                    list: "We embrace innovation and continually seek new ways to enhance the cocktail distribution experience for our users.",
+                },
+                {
+                    listTitle: "Integrity",
+                    list: "We conduct business with honesty, transparency, and integrity, always putting the needs of our users first.",
+                },
+            ],
+        },
+    },
+];
---
+++ pour-distri-frontend/src/assets/data/nav.js
@@ -0,0 +1,14 @@
+export const nav = [
+    {
+        title: "home",
+        path: "/",
+    },
+    {
+        title: "about",
+        path: "/about",
+    },
+    {
+        title: "become a distributor",
+        path: "/apply",
+    },
+];
