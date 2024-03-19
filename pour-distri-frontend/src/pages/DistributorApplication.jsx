a3681bd582078b83d1c506542633401a88d78f7e
Chibuike Mba <chibuike.code@gmail.com>
2024-03-19 22:36:44 +0100

    Update DistributorApplication.jsx

--- pour-distri-frontend/src/pages/DistributorApplication.jsx
+++ pour-distri-frontend/src/pages/DistributorApplication.jsx
@@ -2,31 +2,42 @@ import axios from "axios";
 import ApplicationForm from "../components/ApplicationForm";
 import { toast } from "react-toastify";
 import { redirect, useNavigation } from "react-router";
+import styled from "styled-components";
+
+const ToastWrapper = styled.article`
+    background-image: linear-gradient(to right, #111827, #881337, #6b7280);
+    color: #fff1f2;
+    font-size: 1.05rem;
+    padding: 0.5rem;
+    text-align: center;
+`;
 
 export const action = async ({ request }) => {
     const formDataInstance = await request.formData();
     const applicantsData = Object.fromEntries(formDataInstance);
     try {
         const response = await axios.post(
-            "http://localhost:5001/distributors/applicants",
+            "https://pour-distri.onrender.com/distributors/applicants",
             applicantsData
         );
         toast.success(
-            <p>
-                Your application was{" "}
-                {response.data.applicantStatus.toUpperCase()}!<br />
-                <br />
-                You will be contacted via email if you&apos;re considered.
-            </p>
+            <ToastWrapper>
+                <p>
+                    Your application was{" "}
+                    {response.data.applicantStatus.toUpperCase()}!
+                </p>
+                <p>
+                    You will be contacted via email if you&apos;re considered.
+                </p>
+            </ToastWrapper>
         );
         return redirect("/");
     } catch (error) {
         toast.error(
-            <p>
-                Your application was UNSUCCESSFUL! <br />
-                <br />
-                Please, try again.
-            </p>
+            <ToastWrapper>
+                <p>Your application was UNSUCCESSFUL!</p>
+                <p>Please, try again.</p>
+            </ToastWrapper>
         );
         return error;
     }
